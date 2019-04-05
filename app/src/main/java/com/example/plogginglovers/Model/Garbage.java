9647045756e14894493cbcb8d291c3f3b5649130
Ruben Lauro <ruben.lauro@gmail.com>
2019-04-05 16:29:34 +0100

    Garbage model and RecyclingManager Singleton

---
+++ app/src/main/java/com/example/plogginglovers/Model/Garbage.java
@@ -0,0 +1,29 @@
+package com.example.plogginglovers.Model;
+
+public class Garbage {
+
+    private String garbage;
+
+    private String ecoponto;
+
+    public Garbage(String garbage, String ecoponto) {
+        this.garbage = garbage;
+        this.ecoponto = ecoponto;
+    }
+
+    public String getGarbage() {
+        return garbage;
+    }
+
+    public void setGarbage(String garbage) {
+        this.garbage = garbage;
+    }
+
+    public String getEcoponto() {
+        return ecoponto;
+    }
+
+    public void setEcoponto(String ecoponto) {
+        this.ecoponto = ecoponto;
+    }
+}
---
+++ app/src/main/java/com/example/plogginglovers/Model/RecyclingManager.java
@@ -0,0 +1,47 @@
+package com.example.plogginglovers.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum RecyclingManager {
+    INSTANCE;
+
+    private List<Garbage> garbages;
+
+    RecyclingManager() {
+        this.garbages = new ArrayList<>();
+        //Amarelo
+        this.garbages.add(new Garbage("Latas", "Amarelo"));
+        this.garbages.add(new Garbage("Pacotes", "Amarelo"));
+        this.garbages.add(new Garbage("Aerossóis", "Amarelo"));
+        this.garbages.add(new Garbage("Tabuleiros", "Amarelo"));
+        this.garbages.add(new Garbage("Garrafas", "Amarelo"));
+        this.garbages.add(new Garbage("Garrafões", "Amarelo"));
+        this.garbages.add(new Garbage("Garrafões", "Amarelo"));
+        this.garbages.add(new Garbage("Frascos", "Amarelo"));
+        this.garbages.add(new Garbage("Sacos", "Amarelo"));
+        this.garbages.add(new Garbage("Esferovite", "Amarelo"));
+        //Azul
+        this.garbages.add(new Garbage("Cartão", "Azul"));
+        this.garbages.add(new Garbage("Jornais", "Azul"));
+        this.garbages.add(new Garbage("Revistas", "Azul"));
+        this.garbages.add(new Garbage("Papel", "Azul"));
+        //Verde
+        this.garbages.add(new Garbage("Garrafas", "Verde"));
+        this.garbages.add(new Garbage("Frascos", "Verde"));
+        this.garbages.add(new Garbage("Boiões", "Verde"));
+        //Comum
+        this.garbages.add(new Garbage("Pilhas", "Pilhão"));
+        this.garbages.add(new Garbage("Loiça", "Comum"));
+        this.garbages.add(new Garbage("Comida", "Comum"));
+        this.garbages.add(new Garbage("Janelas", "Comum"));
+        this.garbages.add(new Garbage("Espelhos", "Comum"));
+        this.garbages.add(new Garbage("Lâmpadas", "Comum"));
+
+
+    }
+
+    public List<Garbage> getGarbages() {
+        return garbages;
+    }
+}
